ce5fbcdd6514b10615bfbf22664d4e4896f4d22d
Jewels <49903724+FoxVenture@users.noreply.github.com>
2022-12-13 14:16:28 +0100

    Timelines playable with bool

--- Assets/Scripts/TimelinePlayer.cs
+++ Assets/Scripts/TimelinePlayer.cs
@@ -1,18 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class TimelinePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+    private PlayableDirector director;
+    public GameObject controlPanel;
+    */
+
+    [SerializeField] bool Timeline_1, Timeline_2, Timeline_3 = false;
+    [SerializeField] PlayableDirector TimelineObj_1, TimelineObj_2, TimelineObj_3;
+
+    /*
+    private void Awake()
     {
-        
+        director = GetComponent<PlayableDirector>();
+        director.played += Director_Played;
+        director.stopped += Director_Stopped;
+    }
+    private void Director_Stopped(PlayableDirector obj)
+    {
+        controlPanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Director_Played(PlayableDirector obj)
     {
-        
+        controlPanel.SetActive(false);
+    }
+    */
+
+    public void StartTimeline(PlayableDirector director)
+    {
+        director.Play();
+    }
+
+    private void Update()
+    {
+        if (Timeline_1 == true)
+        {
+            StartTimeline(TimelineObj_1);
+            Timeline_1 = false;
+        }
+
+        if (Timeline_2 == true)
+        {
+            StartTimeline(TimelineObj_2);
+            Timeline_2 = false;
+        }
+
+        if (Timeline_3 == true)
+        {
+            StartTimeline(TimelineObj_3);
+            Timeline_3 = false;
+        }
     }
 }

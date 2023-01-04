7f287c98c28c50ea6fdc99738993dc668296f053
Jewels <j.klomp@student.fontys.nl>
2023-01-04 08:45:08 +0100

    Look at tablet Audio working

--- Assets/Scripts/GameSequence.cs
+++ Assets/Scripts/GameSequence.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class GameSequence : MonoBehaviour
 {
     [SerializeField] AudioManager audio;
-
-    [SerializeField] bool tabletlook;
     [SerializeField] bool audio1, audio2, audio3;
-    [SerializeField] int audioNr=0;
     private string[] audioNPCnames;
+    public bool tabletlook;
 
+    int audioNr = 0;
     int sequenceNr;
 
     private void Start()
@@ -19,68 +18,76 @@ public class GameSequence : MonoBehaviour
         audio2 = false;
         audio3 = false;
         tabletlook = false;
-        audioNPCnames = new string[] { "NPC_audio_1", "NPC_audio_2", "NPC_audio_3" };
+        audioNPCnames = new string[] { "NPC_audio_1", "NPC_audio_2", "NPC_audio_3", "Chris_audio_1", "Chris_audio_2", "Chris_audio_3" };
         sequenceNr = 1;
-        //Sequence_Two();
+        Sequence_Two();
 
     }
-    private bool Sequence_One()
+    private void Sequence_One()
     {
         //play timeline
 
-        return true;
     }
 
-    private bool Sequence_Two()
+    private void Sequence_Two()
     {
-       
-        //ADD WAIT X SECONDS
-        int audioNr = 0;
-
-        while(!tabletlook)
+        if (audioNr == 2)
         {
-            if(audioNr == 2)
-            {
-                playAudio(audioNPCnames[audioNr]);
-                audioNr = 0;
-            }
-            else
-            {
-                playAudio(audioNPCnames[audioNr]);
-                audioNr++;
-            }
-            WaitForRepeat();
+            playAudio(audioNPCnames[audioNr]);
+            audioNr = 0;
+
         }
-
-        return false;
-    }
-
-    private void Update()
-    {
-        if (tabletlook == true)
+        else
         {
-            tabletlook = false;
             playAudio(audioNPCnames[audioNr]);
             audioNr++;
+
         }
+
+        StartCoroutine(WaitForRepeat());
+
+    }
+    public void TabletLookTrue()
+    {
+        tabletlook = true;
     }
 
+    private void Sequence_Three()
+    {
+        if (audioNr == 5)
+        {
+            playAudio(audioNPCnames[audioNr]);
+            audioNr = 3;
+
+        }
+        else
+        {
+            playAudio(audioNPCnames[audioNr]);
+            audioNr++;
+
+        }
+
+        StartCoroutine(WaitForRepeat());
+    }
 
     private void playAudio(string name)
     {
-        PlayWait(audio.Play(name));
-        Debug.Log("TESTING: YAAAY IT WAITED");
+        audio.Play(name);
     }
 
     IEnumerator WaitForRepeat()
     {
         yield return new WaitForSeconds(5);
+        if (!tabletlook)
+        {
+            Sequence_Two();
+        }
+        else
+        {
+            audioNr = 3;
+            tabletlook = false;
+            Sequence_Three();
+        }
     }
 
-    IEnumerator PlayWait(Sound s)
-    {
-        Debug.Log("TESTING: coroutine start");
-        yield return new WaitWhile(() => s.source.isPlaying);
-        Debug.Log("TESTING: coroutine done");
-    }
 }

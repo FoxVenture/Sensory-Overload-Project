a7579111f1e4330da10c42023cb72bb5ab4d3b25
Jewels <j.klomp@student.fontys.nl>
2023-01-05 00:43:39 +0100

    Main Menu working

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -2,17 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuController : MonoBehaviour
+public class MainMenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject tL_1, tL_2;
+    TimelinePlayer tPlayer;
+    AudioManager audioManager;
+    
+
+    public bool buttonClick_1, buttonClick_2;
+
     void Start()
     {
-        
+        tPlayer = GetComponent<TimelinePlayer>();
+        audioManager = GetComponent<AudioManager>();
+        PlayTimeline(tL_1);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if(buttonClick_1 == true)
+        { 
+            buttonClick_1 = false;
+            PlayAudio(1);
+        }
+
+        if(buttonClick_2 == true)
+        {
+            buttonClick_2 = false;
+            PlayTimeline(tL_2);
+        }
+    }
+
+    public void PlayTimeline(GameObject i)
+    {
+        tPlayer.PlayTimeline(i);
+
+    }
+
+    public void PlayAudio(int nr)
+    {
+        StartCoroutine(WaitForAudioPlay(nr));
+    }
+
+
+    IEnumerator WaitForAudioPlay(int i)
+    {
+        yield return new WaitForSeconds(2);
+        audioManager.Play(i);        
     }
 }
